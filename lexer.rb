1479b2e638c0265e3614fff95ea1265c3f7d4635
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2014-11-23 20:44:59 +0100

    Initial lexer implementation.

---
+++ lexer.rb
@@ -0,0 +1,120 @@
+class AssertionError < RuntimeError
+end
+
+def assert &block
+  raise AssertionError unless yield
+end
+
+class Lexer
+
+  def initialize stream, delimiter
+    @stream = stream.chars.to_a
+    @delimiter = delimiter
+  end
+  
+  def each
+    begin
+      token = next_token
+      yield token
+    end while not token.is_a? EOFToken
+  end
+  
+  def next_token
+    char = @stream.shift
+    case char    
+      when eof
+        EOFToken.new
+      when delimiter
+        DelimiterToken.new delimiter
+      when quotes
+        @stream.unshift char
+        get_quoted_identifier
+      else
+        @stream.unshift char
+        get_unquoted_identifier
+    end
+  end
+  
+  def get_unquoted_identifier
+    lexem = ""
+    while true do
+      char = @stream.shift
+      case char
+        when delimiter
+          @stream.unshift char
+          return IdentifierToken.new lexem
+        when eof
+          return IdentifierToken.new lexem
+        else
+          lexem << char
+      end
+    end
+  end
+  
+  def get_quoted_identifier
+    char = @stream.shift
+    assert { char == quotes }
+    lexem = ""
+    while true do
+      char = @stream.shift
+      case char
+        when eof
+          raise "Unexpected EOF within a quoted string."
+        when quotes
+          if @stream.first == quotes
+            lexem << quotes
+            @stream.shift
+           else
+             return IdentifierToken.new lexem
+           end
+        else
+          lexem << char
+      end
+    end
+  end
+  
+  def eof
+    nil
+  end
+  
+  def delimiter
+    @delimiter
+  end
+  
+  def quotes
+    '"'
+  end
+end
+
+class Token
+end
+
+class EOFToken < Token
+  def to_s
+    "EOF"
+  end
+end
+
+class DelimiterToken < Token
+  attr_reader :lexem
+  
+  def initialize lexem
+    @lexem = lexem
+  end
+  
+  def to_s
+    "DELIMITER(#{@lexem})"
+  end
+end
+
+class IdentifierToken < Token
+  attr_reader :lexem
+  
+  def initialize lexem
+    @lexem = lexem
+  end
+  
+  def to_s
+    "IDENTIFIER(#{@lexem})"
+  end
+end
