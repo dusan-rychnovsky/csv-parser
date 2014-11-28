a00999c6ed7b270b8fb02cbed66c413f98a279b4
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2014-11-28 14:31:33 +0100

    Refactoring - use spaces instead of tabs in test cases.

--- test/tc_supports_quoting.rb
+++ test/tc_supports_quoting.rb
@@ -6,47 +6,47 @@ class TestSupportsQuoting < Test::Unit::TestCase
 
   def test_delimiters_are_ingored_inside_quoted_values
   
-	file = StringIO.new(
+    file = StringIO.new(
 'year,make,model,description
 1997,Ford,E350,"Super, luxurious truck"
 2000,Mercury,Cougar,'
-	)
-	
-	csvFile = CsvFile.new.parse file
-
-	row = csvFile[0]
-	assert_equal("1997", row.year)
-	assert_equal("Ford", row.make)
-	assert_equal("E350", row.model)
-	assert_equal("Super, luxurious truck", row.description)
-
-	row = csvFile[1]
-	assert_equal("2000", row.year)
-	assert_equal("Mercury", row.make)
-	assert_equal("Cougar", row.model)
-	assert_equal("", row.description)
+    )
+    
+    csvFile = CsvFile.new.parse file
+    
+    row = csvFile[0]
+    assert_equal("1997", row.year)
+    assert_equal("Ford", row.make)
+    assert_equal("E350", row.model)
+    assert_equal("Super, luxurious truck", row.description)
+    
+    row = csvFile[1]
+    assert_equal("2000", row.year)
+    assert_equal("Mercury", row.make)
+    assert_equal("Cougar", row.model)
+    assert_equal("", row.description)
   end
   
   def test_supports_embedded_quotes
-  
-	file = StringIO.new(
+    
+    file = StringIO.new(
 'year,make,model,description
 1997,Ford,E350,"Super, ""luxurious"" truck"
 2000,Mercury,Cougar,'
-	)
-	
-	csvFile = CsvFile.new.parse file
-
-	row = csvFile[0]
-	assert_equal("1997", row.year)
-	assert_equal("Ford", row.make)
-	assert_equal("E350", row.model)
-	assert_equal("Super, \"luxurious\" truck", row.description)
-
-	row = csvFile[1]
-	assert_equal("2000", row.year)
-	assert_equal("Mercury", row.make)
-	assert_equal("Cougar", row.model)
-	assert_equal("", row.description)
+    )
+    
+    csvFile = CsvFile.new.parse file
+    
+    row = csvFile[0]
+    assert_equal("1997", row.year)
+    assert_equal("Ford", row.make)
+    assert_equal("E350", row.model)
+    assert_equal("Super, \"luxurious\" truck", row.description)
+    
+    row = csvFile[1]
+    assert_equal("2000", row.year)
+    assert_equal("Mercury", row.make)
+    assert_equal("Cougar", row.model)
+    assert_equal("", row.description)
   end
 end
--- test/tc_supports_various_delimiters.rb
+++ test/tc_supports_various_delimiters.rb
@@ -5,119 +5,118 @@ require 'stringio'
 class TestSupportsVariousDelimiters < Test::Unit::TestCase
 
   def test_comma_is_used_as_the_delmiter_by_default
-  
-	file = StringIO.new(
+    
+    file = StringIO.new(
 "year,make,model
 1997,Ford,E350
 2000,Mercury,Cougar"
-	)
-	
-	csvFile = CsvFile.new.parse file
-
-	row = csvFile[0]
-	assert_equal("1997", row.year)
-	assert_equal("Ford", row.make)
-	assert_equal("E350", row.model)
-
-	row = csvFile[1]
-	assert_equal("2000", row.year)
-	assert_equal("Mercury", row.make)
-	assert_equal("Cougar", row.model)
+    )
+    
+    csvFile = CsvFile.new.parse file
+    
+    row = csvFile[0]
+    assert_equal("1997", row.year)
+    assert_equal("Ford", row.make)
+    assert_equal("E350", row.model)
+    
+    row = csvFile[1]
+    assert_equal("2000", row.year)
+    assert_equal("Mercury", row.make)
+    assert_equal("Cougar", row.model)
   end
   
   def test_supports_pipe_as_the_delimiter
-  
-	file = StringIO.new(
+    
+    file = StringIO.new(
 "year|make|model
 1997|Ford|E350
 2000|Mercury|Cougar"
-	)
-	
-	csvFile = CsvFile.new('|').parse file
-
-	row = csvFile[0]
-	assert_equal("1997", row.year)
-	assert_equal("Ford", row.make)
-	assert_equal("E350", row.model)
-
-	row = csvFile[1]
-	assert_equal("2000", row.year)
-	assert_equal("Mercury", row.make)
-	assert_equal("Cougar", row.model)
+    )
+    
+    csvFile = CsvFile.new('|').parse file
+    
+    row = csvFile[0]
+    assert_equal("1997", row.year)
+    assert_equal("Ford", row.make)
+    assert_equal("E350", row.model)
+    
+    row = csvFile[1]
+    assert_equal("2000", row.year)
+    assert_equal("Mercury", row.make)
+    assert_equal("Cougar", row.model)
   end
   
   def test_supports_semicolon_as_the_delimiter
-  
-	file = StringIO.new(
+    
+    file = StringIO.new(
 "year;make;model
 1997;Ford;E350
 2000;Mercury;Cougar"
-	)
-	
-	csvFile = CsvFile.new(';').parse file
-
-	row = csvFile[0]
-	assert_equal("1997", row.year)
-	assert_equal("Ford", row.make)
-	assert_equal("E350", row.model)
-
-	row = csvFile[1]
-	assert_equal("2000", row.year)
-	assert_equal("Mercury", row.make)
-	assert_equal("Cougar", row.model)
+    )
+    
+    csvFile = CsvFile.new(';').parse file
+    
+    row = csvFile[0]
+    assert_equal("1997", row.year)
+    assert_equal("Ford", row.make)
+    assert_equal("E350", row.model)
+    
+    row = csvFile[1]
+    assert_equal("2000", row.year)
+    assert_equal("Mercury", row.make)
+    assert_equal("Cougar", row.model)
   end
   
   def test_supports_space_as_the_delimiter
-  
-	file = StringIO.new(
+    
+    file = StringIO.new(
 "year make model
 1997 Ford E350
 2000 Mercury Cougar"
-	)
-	
-	csvFile = CsvFile.new(' ').parse file
-
-	row = csvFile[0]
-	assert_equal("1997", row.year)
-	assert_equal("Ford", row.make)
-	assert_equal("E350", row.model)
-
-	row = csvFile[1]
-	assert_equal("2000", row.year)
-	assert_equal("Mercury", row.make)
-	assert_equal("Cougar", row.model)
+    )
+    
+    csvFile = CsvFile.new(' ').parse file
+    
+    row = csvFile[0]
+    assert_equal("1997", row.year)
+    assert_equal("Ford", row.make)
+    assert_equal("E350", row.model)
+    
+    row = csvFile[1]
+    assert_equal("2000", row.year)
+    assert_equal("Mercury", row.make)
+    assert_equal("Cougar", row.model)
   end
   
-  
   def test_supports_tab_as_the_delimiter
-  
-	file = StringIO.new(
+    
+    file = StringIO.new(
 "year	make	model
 1997	Ford	E350
 2000	Mercury	Cougar"
-	)
-	
-	csvFile = CsvFile.new('	').parse file
-
-	row = csvFile[0]
-	assert_equal("1997", row.year)
-	assert_equal("Ford", row.make)
-	assert_equal("E350", row.model)
-
-	row = csvFile[1]
-	assert_equal("2000", row.year)
-	assert_equal("Mercury", row.make)
-	assert_equal("Cougar", row.model)
+    )
+    
+    csvFile = CsvFile.new('	').parse file
+    
+    row = csvFile[0]
+    assert_equal("1997", row.year)
+    assert_equal("Ford", row.make)
+    assert_equal("E350", row.model)
+    
+    row = csvFile[1]
+    assert_equal("2000", row.year)
+    assert_equal("Mercury", row.make)
+    assert_equal("Cougar", row.model)
   end
   
   def test_does_not_support_dash_as_the_delimiter
-  
-  file = StringIO.new(
+    
+    file = StringIO.new(
 "year-make-model
 1997-Ford-E350
 2000-Mercury-Cougar"
-	)
-	
-	assert_raise(UnsupportedDelimiterException) { CsvFile.new '-' }
+    )
+    
+    assert_raise(UnsupportedDelimiterException) { CsvFile.new '-' }
   end
 end
