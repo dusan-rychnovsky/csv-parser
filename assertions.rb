1cc9e294b9b0e4e7f451432576fe50d44424aa78
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2014-11-28 14:26:48 +0100

    Add some more comments.

--- assertions.rb
+++ assertions.rb
@@ -1,6 +1,11 @@
+# An exception to represent assertion violations.
+#
 class AssertionError < RuntimeError
 end
 
+# Evaluates the given block as a boolean expression and throws an AssertionError
+# in case it results to false.
+#
 def assert &block
   raise AssertionError unless yield
 end
